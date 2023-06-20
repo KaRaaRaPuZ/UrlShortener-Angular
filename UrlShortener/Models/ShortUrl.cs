34612e5def1b612c9382e3d91103d7d8115f1605
MoonLightEagle <danylosokoliuk13@gmail.com>
2023-06-20 22:04:25 +0300

    Work in progress...

---
+++ UrlShortener/Models/ShortUrl.cs
@@ -0,0 +1,11 @@
+﻿namespace UrlShortener.Models
+{
+    public class ShortUrl
+    {
+        public int Id { get; set; }  // Primary Key
+        public string OriginalUrl { get; set; }
+        public string ShortenedUrl { get; set; }
+        public string CreatedBy { get; set; }  // The user who created the URL
+        public DateTime CreatedDate { get; set; }
+    }
+}
